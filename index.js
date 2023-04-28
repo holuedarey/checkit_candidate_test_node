fe61c8101ee70523f3d50084ce664e741cd667db
ogunwole samuel <holudare2076@gmail.com>
2023-04-28 12:19:33 +0100

    add comment to code flow for easy documentation

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
+//initialize the data from data.json
 const letaShop = require('./data.json');
 
+//array of data for customers 
 const customers = letaShop['customers'] ?? [];
+
+//array of data for purchases 
 let purchases = letaShop['purchases'];
 
 
-const customerWithPurchase = customers.filter(customer => purchases.find(purchase => purchase.customerId.includes(customer.id)));
+/**
+ * filter out customer base on customerId and customer id exist on purchase
+ */
+const customerWithPurchase = customers?.filter(customer => purchases?.find(purchase => purchase?.customerId.includes(customer?.id)));
 
 console.log("Output the customers who has made a purchase with Leta and output the customer information.");
 console.log("--------------------------------------------------------------------------------------------");
@@ -13,10 +20,15 @@ console.log("-------------------------------------------------------------------
 
 
 
+/**
+ * filter purchase greater than one and map out only the customerId to use for the next step
+ */
+const purchaseGreaterThanOne = purchases?.filter((purchase) => purchase?.purchases.length > 1).map(entry => entry?.customerId);
 
-const purchaseGreaterThanOne = purchases.filter((purchase) => purchase['purchases'].length > 1).map(entry => entry['customerId']);
-
-const customersPurchase = customers.filter(customer =>  purchaseGreaterThanOne.includes(customer.id) );
+/**
+ * filter customer with customer Id who has more than one purchase from the initial step 
+ */
+const customersPurchase = customers.filter(customer =>  purchaseGreaterThanOne?.includes(customer?.id) );
 
 console.log("Find the customers who have purchased any gaming product more than once and output the customer information.");
 console.log("--------------------------------------------------------------------------------------------------------------");
@@ -24,14 +36,20 @@ console.log(customersPurchase);
 console.log("--------------------------------------------------------------------------------------------------------------");
 
 
+//declare a variable to hold the result of the proccess
 let  results = [];
-customers.map((customer) => {
-    return letaShop['products'].map((product) =>  {
-        const numberOfPurchases =  (purchases.find((purchase) => customer.id === purchase.customerId));
+
+/**
+ * step One map throuh the customer 
+ * step two map through the product to get the purchase base on each customer Id and product Id from Purchases
+ */
+customers?.map((customer) => {
+    return letaShop?.products.map((product) =>  {
+        const numberOfPurchases =  (purchases?.find((purchase) => customer?.id === purchase?.customerId));
        results.push({
             "customerName": `${customer['firstname']} ${customer['lastname']}`,
             "productName": product['name'],
-            "numberOfPurchases": numberOfPurchases ? (numberOfPurchases.purchases.filter(purchaseEntry => purchaseEntry.productId === product.id)).length : 0 
+            "numberOfPurchases": numberOfPurchases ? (numberOfPurchases?.purchases.filter(purchaseEntry => purchaseEntry?.productId === product?.id)).length : 0 
           });
     })
     
